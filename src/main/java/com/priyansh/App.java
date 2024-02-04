531197ca1452e346f14a06b4495c1abf796f115a
priyansh <pkpandeyji28@gmail.com>
2024-02-04 14:43:38 +0530

    first Commit

---
+++ src/main/java/com/priyansh/App.java
@@ -0,0 +1,13 @@
+package com.priyansh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
